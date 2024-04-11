b9384a59ca80f60c6320737227ec97746fa49dfa
pushkar132 <pushkarlunawat1032002@gmail.com>
2024-04-11 16:43:15 +0530

    Added User Entity

---
+++ src/main/java/com/infosysSpringboard/flightManagementSystem/entity/User.java
@@ -0,0 +1,112 @@
+package com.infosysSpringboard.flightManagementSystem.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int userId;
+
+    @Column(name = "user_type")
+    private String userType;
+
+    @Column(name = "user_name")
+    private String userName;
+
+    @Column(name = "user_password")
+    private String userPassword;
+
+    @Column(name = "user_phone")
+    private String userPhone;
+
+    @Column(name = "user_email")
+    private String userEmail;
+
+    public User(int userId, String userType, String userName, String userPassword, String userPhone, String userEmail) {
+        this.userId = userId;
+        this.userType = userType;
+        this.userName = userName;
+        this.userPassword = userPassword;
+        this.userPhone = userPhone;
+        this.userEmail = userEmail;
+    }
+
+    public User() {
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getUserType() {
+        return userType;
+    }
+
+    public void setUserType(String userType) {
+        this.userType = userType;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserPassword() {
+        return userPassword;
+    }
+
+    public void setUserPassword(String userPassword) {
+        this.userPassword = userPassword;
+    }
+
+    public String getUserPhone() {
+        return userPhone;
+    }
+
+    public void setUserPhone(String userPhone) {
+        this.userPhone = userPhone;
+    }
+
+    public String getUserEmail() {
+        return userEmail;
+    }
+
+    public void setUserEmail(String userEmail) {
+        this.userEmail = userEmail;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "userId=" + userId +
+                ", userType='" + userType + '\'' +
+                ", userName='" + userName + '\'' +
+                ", userPassword='" + userPassword + '\'' +
+                ", userPhone='" + userPhone + '\'' +
+                ", userEmail='" + userEmail + '\'' +
+                '}';
+    }
+}
+/*
+Attributes:
+userType:
+String. userId:
+BigInteger
+userName:
+String
+userPassword:
+Password
+userPhone:BigI
+nteger
+userEmail:
+String
+ */
